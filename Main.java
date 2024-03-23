ceb49aa8b359d341878607f6dcc795c3dff08c9f
priyanshu5656 <priyanshukumar906@gmail.com>
2024-03-23 23:28:05 +0530

    making all classes

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;    
+import javax.swing.*; 
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+class main{
+    public static void main(String args[]) throws IOException{
+        Map<String,Batch> batch = new HashMap<String,Batch>();
+        Map<String,Course> course = new HashMap<String,Course>();
+        Map<String,Professor> professor= new HashMap<String,Professor>();
+    }
+    
+}
